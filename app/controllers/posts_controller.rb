934ba23b146dc1ca0f8897d8c15a4dcdf603be80
Brandon Weaver <a7xncob@gmail.com>
2020-01-30 20:14:40 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,11 @@ class PostsController < ApplicationController
     if !params[:author].blank?
       @posts = Post.where(author: params[:author])
     elsif !params[:date].blank?
-    
+      if params[:date] == "Today"
+        @posts = Post.where("created_at >= ?", Time.zone.today.beginning_of_day)
+      else
+        @posts = Post.where("created_at < ?", Time.zone.today.beginning_of_day)
+      end
     else
       @posts = Post.all
     end
